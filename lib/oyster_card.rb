afd05f4d53d7a5812cad2a03c2c889c6540ea969
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-04-18 21:22:25 +0100

    Adds code up to Challenge 10

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,15 +1,37 @@
 class Oystercard
-  attr_reader :balance
-  BALANCE_LIMIT = 90
+attr_reader :balance
+
 def initialize
     @balance = 0
 end
 
+BALANCE_LIMIT = 90
+MINIMUM_BALACE = 1
+
 def top_up(value)
   if (@balance + value) > BALANCE_LIMIT
-    fail "New balance would exceed set limit of #{BALANCE_LIMIT}. You may insert up to #{BALANCE_LIMIT - @balance}}"
+    fail "New balance would exceed set limit of #{BALANCE_LIMIT} You may insert up to #{BALANCE_LIMIT - @balance}."
   else
     @balance += value
     end
+end
+
+def deduct(value)
+    @balance -= value
+end
+def touch_in
+  if @balance < 1
+    fail "Not enough money on the card"
+  else
+    @journey = true
   end
+end
+
+def touch_out
+  @journey = false
+end
+
+def in_journey?
+    @journey
+end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,17 +1,47 @@
 require 'oyster_card'
+
 describe Oystercard do
  it 'has a balance of 0' do
    expect(subject.balance).to eq(0)
  end
-   describe '#top_up' do
+describe '#top_up' do
      it 'Updates the balance'do
-   subject.top_up(20)
-   expect(subject.balance).to eq(20)
-    end
+      subject.top_up(20)
+      expect(subject.balance).to eq(20)
+ end
       it 'Passes an error if the value on the card would exceed 90' do
         max_balance = Oystercard::BALANCE_LIMIT
-        expect {subject.top_up(max_balance + 1)}.to raise_error "New balance would exceed set limit of #{BALANCE_LIMIT}. You may insert up to #{BALANCE_LIMIT - @balance}}"
+        expect{subject.top_up(max_balance + 1)}.to raise_error "New balance would exceed set limit of #{BALANCE_LIMIT}. You may insert up to #{BALANCE_LIMIT - @balance}."
     end
   end
+describe '#deduct'do
+      it 'deducts money from the card'do
+        subject.top_up(50)
+        expect(subject.deduct(20)).to eq(30)
+      end
+end
+describe '#in_journey?'do
+  it 'is not in a journey'do
+    expect(subject).not_to be_in_journey
+  end
+end
+describe '#touch_in'do
+  it 'Touches in' do
+    subject.top_up(MINIMUM_BALACE)
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+  it 'Error if card has too little money'do
+    expect{subject.touch_in}.to raise_error "Not enough money on the card"
+  end
+end
 
- end
+describe '#touch_out'do
+  it "Can touch_out" do
+    subject.top_up(MINIMUM_BALACE)
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+end
+end
